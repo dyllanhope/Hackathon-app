e142a6c0c719d6f02a8e478fbefe46c2b9dadde7
WNortier <wnortier@gmail.com>
2019-10-31 12:10:20 +0200

    setting up routes

--- routes.js
+++ routes.js
@@ -1,8 +1,32 @@
 const AddRecord = require('./services/addRecord');
+const DataStorage = require('./services/dataStorage')
 module.exports = (app, pool) => {
     const addRecord = AddRecord(pool);
+    const dataStorage = DataStorage();
 
-    app.get('/', (req, res) => {
-        res.send('Hello World');
-    })
+    //app.get('submission', res.render)
+
+
+    app.get('/statistics', async (req, res, next) => {
+
+
+        res.render("statistics", {
+            statsInfo: await dataStorage.dataReturner()
+        });
+
+    });
+
+
+    app.get('/submission', async (req, res, next) => {
+        res.render("submission");
+    });
+
+    app.get('/submission/one', async (req, res, next) => {
+        res.render("submission");
+    });
+
+return {
+    displaySubmission,
+    displayStatistics
+}
 }
